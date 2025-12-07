d7d33bd6f14d088fc03075a7bf7190f65aee4b03
balamurugan-testpress <balamurugan@testpress.in>
2025-12-07 19:15:10 +0530

    samp

---
+++ scripts/install-NFS.sh
@@ -0,0 +1,7 @@
+sudo tee /etc/apt/sources.list >/dev/null <<EOF
+deb http://archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu jammy-security main restricted universe multiverse
+EOF
+
